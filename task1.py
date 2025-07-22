228ef55c0656b5850dde48d98f21ec0a33088c8f
Harsh Srivastava <119413903+harsh0544@users.noreply.github.com>
2025-07-22 22:13:15 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,18 +1,6 @@
-num1 = float(input("Enter a number: "))
-num2 = float(input("Enter another number: "))
+num = int(input("Enter an Number: "))
 
-Addition = num1 + num2
-Subtraction = num1 - num2
-Multiplication = num1 * num2
-
-print("Enter The First Number:", num1)
-print("Enter The Second Number:", num2)
-
-print("Addition", Addition)
-print("Subtraction:", Subtraction)
-print("Multiplication:", Multiplication)
-if num2 != 0:
-    print(f"Division: {num1 / num2}")
+if num % 2 == 0:
+    print(f"{num} is even number.")
 else:
-    print("Cannot divide by zero.")
-
+    print(f"{num} is odd number.")

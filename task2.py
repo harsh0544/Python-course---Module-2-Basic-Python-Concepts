879949859a510b70f2a4032b7c3c577d0a9ff312
Harsh Srivastava <119413903+harsh0544@users.noreply.github.com>
2025-08-02 13:06:59 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,14 +1,9 @@
-user_input = input("Enter some text to write to the file: ")
+numbers = list(range(1, 11))
 
-with open("output.txt", "w") as file:
-    file.write(user_input + "\n")  
+first_five = numbers[:5]
 
+reversed_five = first_five[::-1]
 
-additional_data = input("Enter additional text to append to the file: ")
-
-with open("output.txt", "a") as file:
-    file.write(additional_data + "\n") 
-
-with open("output.txt", "r") as file:
-    print("\nFinal content of the file:")
-    print(file.read())  
+print("Original:",numbers)
+print("First five elements:", first_five)
+print("Reversed list:", reversed_five)
